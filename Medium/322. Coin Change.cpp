a558ca8013cd90ffb967c99f915f4d0f5abec4f1
Damien <81557462+Damien-Chen@users.noreply.github.com>
2022-08-15 10:28:43 +0800

    Update 322. Coin Change.cpp

--- Medium/322. Coin Change.cpp
+++ Medium/322. Coin Change.cpp
@@ -1,5 +1,18 @@
 class Solution {
 public:
+    
+    # Using DP wiht memoization and recursion
+    # {1,3,4,5} amount = 7
+    # dp[0] = 0, dp[1] = 1
+    # dp[2] = 1 + dp[1] = 2
+    # dp[3] = 1, dp[4] = 1 
+    # dp[5] = 1, dp[6] = 1 + dp[5] = 2
+    # dp[7] = dp[1] + dp[6] = 3
+    # dp[7] = dp[3] + dp[4] = 2
+    # dp[7] = dp[4] + dp[3] = 2
+    # dp[7] = dp[5] + dp[2] = 3
+    # return 2
+    
     int Solve(vector<int> coins, int amount, vector<int> &dp){
         if(amount < 0 )
             return 100000;
